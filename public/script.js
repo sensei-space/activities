cfcd15269c85d2c7cbc024923c1c262215a9e624
mirko.gelsomini <mirko.gelsomini@supsi.ch>
2025-02-03 18:54:14 +0100

    feat: implement localization for activity titles and enhance layout with title container

--- public/script.js
+++ public/script.js
@@ -21,83 +21,169 @@ let defaultPalette = [
   { color: "#000000", label: "Black" }, // Black
 ];
 
-let parameters = {
-  "en": "Parameters",
-  "it": "Parametri",
-  "fr": "Paramètres",
-  "es": "Parámetros",
-  "de": "Parameter"
-}
+let dictionary = {
+  title: {
+    en: "Available activities in",
+    it: "Attività disponibili in",
+    fr: "Activités disponibles en",
+    es: "Actividades disponibles en",
+    de: "Verfügbare Aktivitäten in",
+  },
+  parameters: {
+    en: "Parameters",
+    it: "Parametri",
+    fr: "Paramètres",
+    es: "Parámetros",
+    de: "Parameter",
+  },
+  yesno: {
+    en: "Yes/No",
+    it: "Sì/No",
+    fr: "Oui/Non",
+    es: "Sí/No",
+    de: "Ja/Nein",
+  },
+  button: {
+    en: "Button",
+    it: "Pulsante",
+    fr: "Bouton",
+    es: "Botón",
+    de: "Schaltfläche",
+  },
+  image: {
+    en: "Local or internet image",
+    it: "Immagine locale o da internet",
+    fr: "Image locale ou internet",
+    es: "Imagen local o de internet",
+    de: "Lokales oder Internetbild",
+  },
+  video: {
+    en: "Local or internet video",
+    it: "Video locale o da internet",
+    fr: "Vidéo locale ou internet",
+    es: "Video local o de internet",
+    de: "Lokales oder Internetvideo",
+  },
+  audio: {
+    en: "Local or internet audio",
+    it: "Audio locale o da internet",
+    fr: "Audio local ou internet",
+    es: "Audio local o de internet",
+    de: "Lokales oder Internet-Audio",
+  },
+  from: {
+    en: "from",
+    it: "da",
+    fr: "de",
+    es: "de",
+    de: "von",
+  },
+  to: {
+    en: "to",
+    it: "a",
+    fr: "à",
+    es: "a",
+    de: "zu",
+  },
+};
+
+let activitiesContainer;
+
+
+disableRightClick();
 
 document.addEventListener("DOMContentLoaded", function () {
+
+  document.getElementById("title").innerText = t("title");
+
+  activitiesContainer = document.getElementById("activitiesList");
+  loadJson();
+});
+
+function loadJson() {
   fetch("./data/list.json")
     .then((response) => response.json())
     .then((data) => {
       const activities = data.activities || [];
-      const activitiesContainer = document.getElementById("activitiesList");
 
       activities.forEach((activity) => {
-        if (activity.info.locales && locale in activity.info.locales) {
-          const title = activity.info.locales[locale].title || "Untitled";
-          const description =
-            activity.info.locales[locale].description ||
-            "No description available";
-          let icon = "./data/activities/" + activity.id + ".png";
-
-          const activityDiv = document.createElement("div");
-          activityDiv.classList.add("activity");
-
-          let parametersHTML = `<details><summary>${parameters[locale]}</summary><ul>`;
-          (activity.parameters || []).forEach((param) => {
-            switch (param.type) {
-              case "toggle":
-                parametersHTML += `<li><b>${param.text}</b>: ${
-                  param.default === "true" ? "Sì" : "No"
-                }</li>`;
-                break;
-              case "button":
-                parametersHTML += `<li><b>${param.text}</b>: <div class="button">${
-                  param.label || "Button"
-                }</div></li>`;
-                break;
-              case "slider":
-                parametersHTML += `<li><b>${param.text}</b>: Da ${param.min} a ${param.max}</li>`;
-                break;
-              case "list":
-                parametersHTML += `<li><b>${param.text}</b>: <ul>`;
-                param.choices.forEach((choice) => {
-                  parametersHTML += `<li>${choice}</li>`;
-                });
-                parametersHTML += `</ul></li>`;
-                break;
-              case "color":
-                parametersHTML += `<li><b>${param.text}</b>: `;
-                parametersHTML += createPalette(param);
-                parametersHTML += `</li>`;
-                break;
-            }
-          });
-          parametersHTML += "</ul></details>";
-
-          activityDiv.innerHTML = `
-                        <div class="image">
-                            <img src="${icon}" alt="${title}">
-                        </div>
-                        <div class="info">
-                            <h3>${title}</h3>
-                            <p>${description}</p>
-                            ${parametersHTML}
-                        </div>
-                    `;
-
-          activitiesContainer.appendChild(activityDiv);
-        } else {
-          console.log("No locale found for activity", activity.id);
-        }
+        addActivity(activity);
       });
     })
     .catch((error) => console.error("Error loading list.json:", error));
-});
+}
+
+function addActivity(activity) {
+  if (activity.info.locales && locale in activity.info.locales) {
+    const title = activity.info.locales[locale].title || "";
+    if(title === "") return;
+
+    const description = activity.info.locales[locale].description || "";
+    let icon = "./data/activities/" + activity.id + ".png";
+
+    const activityDiv = document.createElement("div");
+    activityDiv.classList.add("activity");
+
+    //set attributes in data-
+    activityDiv.setAttribute("data-id", activity.id);
+    activityDiv.setAttribute("data-title", title);
+
+    let parametersHTML = `<details><summary>${t("parameters")}</summary><ul>`;
+    (activity.parameters || []).forEach((param) => {
+      switch (param.type) {
+        case "toggle":
+          parametersHTML += `<li><b>${param.text}</b>: ${t("yesno")}</li>`;
+          break;
+        case "button":
+          parametersHTML += `<li><b>${param.text}</b>: <div class="button">${
+            param.label || t("button")
+          }</div></li>`;
+          break;
+        case "slider":
+          parametersHTML += `<li><b>${param.text}</b>: ${t("from")} ${param.min} ${t("to")} ${param.max}</li>`;
+          break;
+        case "list":
+          parametersHTML += `<li><b>${param.text}</b>: <ul>`;
+          param.choices.forEach((choice) => {
+            parametersHTML += `<li>${choice}</li>`;
+          });
+          parametersHTML += `</ul></li>`;
+          break;
+        case "color":
+          parametersHTML += `<li><b>${param.text}</b>: `;
+          parametersHTML += createPalette(param);
+          parametersHTML += `</li>`;
+          break;
+        case "image":
+          parametersHTML += `<li><b>${param.text}</b>: ${t("image")}</li>`;
+          break;
+        case "video":
+          parametersHTML += `<li><b>${param.text}</b>: ${t("video")}</li>`;
+          break;
+        case "audio":
+          parametersHTML += `<li><b>${param.text}</b>: ${t("audio")}</li>`;
+          break;
+      }
+    });
+    parametersHTML += "</ul></details>";
+
+    activityDiv.innerHTML = `
+                  <div class="image">
+                      <img src="${icon}" alt="${title}">
+                  </div>
+                  <div class="info">
+                      <h3>${title}</h3>
+                      <p>${description}</p>
+                      ${parametersHTML}
+                  </div>
+              `;
+
+    activitiesContainer.appendChild(activityDiv);
+  } else {
+    console.log("No locale found for activity", activity.id);
+  }
+}
+
 function createPalette(param) {
   let palette = [];
   let defaultColor = param.default || "#FFFFFF";
@@ -139,11 +225,24 @@ function createPalette(param) {
   // Generate HTML for the color palette
   let paletteHTML = `<div class="palette">`;
   palette.forEach((color) => {
-    paletteHTML += `<div style="background-color:${
-      color.color
-    }" class="color ${color.selected ? "selected" : ""}"></div>`;
+    paletteHTML += `<div style="background-color:${color.color}" class="color ${
+      color.selected ? "selected" : ""
+    }"></div>`;
   });
   paletteHTML += `</div>`;
 
   return paletteHTML;
+}
+
+function t(key) {
+  if (key in dictionary && locale in dictionary[key]) {
+    return dictionary[key][locale];
+  }
+  return "";
+}
+
+function disableRightClick() {
+  document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+  });
 }
